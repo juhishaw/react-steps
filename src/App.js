325e251b09e8aa516f46c08c976c460ad5e46177
juhi shaw <jshaw@ciena.com>
2023-10-09 12:37:53 +0530

    add files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+const App = () => {
+  // const step = 1;
+
+  //useState --> Default Value, updateState
+  //They are hooks, any keyword starting with 'use' is a hook
+  //Can only be called inside react component, will give error if called inside if or normal functions
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  const handlePrevious = () => {
+    if (step > 1) setStep(step - 1);
+  };
+
+  const handleNext = () => {
+    if (step < 3) {
+      /*
+        This won't work, step !== 3, as update state doesnot take current value like this.
+        For making it work, we have to use callback
+        This is a more safe way
+      */
+      // setStep(step + 1)
+      // setStep(step + 1)
+      setStep((currentStep) => currentStep + 1);
+      // setStep((currentStep) => currentStep + 1)
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <button onClick={() => setIsOpen((is) => !is)} className="close">
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step === 1 ? "active" : ""}>1</div>
+            <div className={step === 2 ? "active" : ""}>2</div>
+            <div className={step === 3 ? "active" : ""}>3</div>
+          </div>
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
+          <div className="buttons">
+            <Button textColor="#fff" bgColor="#7950f2" onClick={handlePrevious}>
+              <span>👈🏻</span> Previous
+            </Button>
+            <Button
+              textColor="#fff"
+              bgColor="#7950f2"
+              onClick={handleNext}
+              text="Next"
+              emoji="👉🏻"
+            >
+              Next <span>👉🏻</span>
+            </Button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+const StepMessage = ({ step, children }) => {
+  return (
+    <div className="message">
+      <h3>Step {step}</h3>
+      {children}
     </div>
   );
-}
+};
+
+const Button = ({ textColor, bgColor, onClick, children }) => {
+  return (
+    <button
+      onClick={onClick}
+      style={{ backgroundColor: bgColor, color: textColor }}
+    >
+      {children}
+    </button>
+  );
+};
 
 export default App;
